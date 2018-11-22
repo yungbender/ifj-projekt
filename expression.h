b29897c836f4932993da3420a50970c0b0fbfade
TomasVenkrbec <32966172+TomasVenkrbec@users.noreply.github.com>
2018-11-22 19:38:46 +0100

    Added expression.h

---
+++ expression.h
@@ -0,0 +1,8 @@
+int table_index(tToken token);
+void reduce_by_rule(tStack *tmp_stack);
+void opt_reduce();
+void opt_switch();
+void insert_list(tTList **list, tToken token);
+void copy_list_to_stack(tTList *list);
+void opt_eq();
+void pars_expression();
